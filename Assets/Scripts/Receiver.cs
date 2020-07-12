f48071f8c8fd0cd5e8db9f0c27030cdfe1b0182e
FrancoMoG <franco.aresv@gmail.com>
2020-07-12 13:18:09 -0300

    TODO UNDO AND LEVELS Y MERGER

--- Assets/Scripts/Receiver.cs
+++ Assets/Scripts/Receiver.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class Receiver : MovementObject
 {
     public Color32 clrSolution;
-    private Color32 clrStart;
+    private Color32 clrStart, clrStartLight, clrStartFlare;
     private bool bIsSolved = false;
     public Renderer recPetalRenderer;
-    public Renderer recPotRenderer;
+    public Renderer recLamp;
 
     private const int CENTER_RENDERER_MATERIAL = 0;
     private const int PETAL_RENDERER_MATERIAL = 1;
-    private const int POT_RENDERER_MATERIAL = 0;
+    private const int LAMP_RENDERER_MATERIAL = 0;
+    private const int LAMP_LIGHT_RENDERER_MATERIAL = 1;
 
     private const float UNSOLVE_EVERY_SEC = 1f;
 
@@ -27,6 +28,9 @@ public class Receiver : MovementObject
         base.Start();
 
         clrStart = recPetalRenderer.materials[PETAL_RENDERER_MATERIAL].GetColor("_Color");
+        clrStartLight = recLamp.materials[LAMP_LIGHT_RENDERER_MATERIAL].GetColor("_Color");
+        clrStartFlare = recLamp.GetComponentInChildren<Light>().color;
+        recLamp.GetComponentInChildren<Light>().enabled = false;
         UpdateColorSolution(clrSolution);
     }
 
@@ -35,7 +39,7 @@ public class Receiver : MovementObject
     {
         base.Update();
 
-        if (bIsSolved)
+        //if (bIsSolved)
         {
             if (fTimeSinceLastSolve >= UNSOLVE_EVERY_SEC)
             {
@@ -48,7 +52,7 @@ public class Receiver : MovementObject
     public void UpdateColorSolution(Color32 clr)
     {
         clrSolution = clr;
-        recPotRenderer.materials[POT_RENDERER_MATERIAL].SetColor("_Color", clrSolution);
+        recLamp.materials[LAMP_RENDERER_MATERIAL].SetColor("_Color", clrSolution);
     }
 
     public bool IsSolved()
@@ -60,8 +64,10 @@ public class Receiver : MovementObject
     {
         fTimeSinceLastSolve = 0;
         fTimeSolving += MMConstants.TIME_TO_TRYTOSOLVE_AGAIN;
+
         if (fTimeSolving >= TIME_TO_SOLVE)
         {
+            recLamp.GetComponentInChildren<Light>().enabled = true;
             recPetalRenderer.materials[PETAL_RENDERER_MATERIAL].SetColor("_Color", clrSolution);
             bIsSolved = true;
         }
@@ -72,11 +78,18 @@ public class Receiver : MovementObject
         fTimeSinceLastSolve = 0;
         fTimeSolving = 0;
         recPetalRenderer.materials[PETAL_RENDERER_MATERIAL].SetColor("_Color", clrStart);
+        recLamp.materials[LAMP_LIGHT_RENDERER_MATERIAL].SetColor("_Color", clrStartLight);
+        recLamp.GetComponentInChildren<Light>().enabled = false;
+
+        recLamp.GetComponentInChildren<Light>().color = clrStartFlare;
+
         bIsSolved = false;
     }
 
     public void TryToSolve(Color32 clr)
     {
+        recLamp.materials[LAMP_LIGHT_RENDERER_MATERIAL].SetColor("_Color", clr);
+        recLamp.GetComponentInChildren<Light>().color = clr;
         if (clr.Equals(clrSolution))
         {
             Solve();
